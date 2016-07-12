dc15141b6c5762af52bafdc72419c6b09ab47d44
blake nicholson <blake nicholson>
2016-07-12 17:29:35 -0400

    worked on routes

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -10,5 +10,20 @@ router.get('/issues', function(req, res, next) {
   res.render('issues', { title: 'Curt Nicholson' });
 });
 
+router.get('/why', function(req, res, next) {
+  res.render('why', { title: 'Curt Nicholson' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Curt Nicholson' });
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'Curt Nicholson' });
+});
+
+router.get('/support', function(req, res, next) {
+  res.render('support', { title: 'Curt Nicholson' });
+});
 
 module.exports = router;
