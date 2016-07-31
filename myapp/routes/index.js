7a2339c612b1ba648ee77e206332613e4e5bc42e
blake nicholson <blake nicholson>
2016-07-31 12:22:14 -0400

    added nodemailer and post route

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,6 +25,32 @@ router.get('/blog', function(req, res, next) {
 
 router.get('/support', function(req, res, next) {
   res.render('support', { title: 'Curt Nicholson' });
+});
+/* Post Email contact */
+router.post('/contact/send', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'curtnicholsonwebsite@gmail.com',
+			pass: 'BNfdh6774@#$%'
+		}
+	});
+	var mailOptions = {
+		from: 'Curt Nick <curtnicholsonwebsite@gmail.com>',
+		to: 'curtnicholsonwebsite@gmail.com',
+		subject: 'Website submission',
+		text: 'You have a submission from the website Name:' +req.body.firstname+'Email: ' +req.body.email+'Message: ' +req.body.message,
+		html: '<p>You have a submission with the following details</p><ul><li>Name: '+req.body.firstname+req.body.lastname+'</li><li>Email: ' +req.body.email+'</li><li>Message: ' +req.body.message+'</li></ul>'
+	};
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		}else{
+			console.log('message sent:' +info.response);
+			res.redirect('/');
+		}
+	});
 });
 
 module.exports = router;
