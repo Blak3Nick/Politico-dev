55e70ef25e640fbc0cf9f0afa96d9a9757fea7a3
Blake Nicholson <blaken2021@gmail.com>
2016-08-08 18:46:47 -0400

    added box shadow

--- myapp/public/javascripts/main.js
+++ myapp/public/javascripts/main.js
@@ -1,14 +1,21 @@
 
+var topnav = document.getElementsByClassName('topnav');
 function myFunction() {
     document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
 }
-// $(document).ready(function() {
-//
-//   $(window).scroll(function() {
-//     console.log($(window).scrollTop())
-//     if ($(window).scrollTop() > 280) {
-//       $('.navbar').removeClass('navbar-default');
-//       $('.navbar').addClass('navbar-inverse');
-//     }
-//   });
-// });
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $(".maintext");
+    var offset = startchange.offset();
+
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $(topnav).css('background-color', 'rgba(0,0,0,.75)');
+            } else {
+                $(topnav).css('background-color', 'transparent');
+            }
+        });
+    }
+});
