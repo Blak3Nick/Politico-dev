e5384c8fc4f876d812a377b0554e4e12f5fddc96
Blake Nicholson <blaken2021@gmail.com>
2016-08-10 17:49:25 -0400

    new wording

--- myapp/public/javascripts/main.js
+++ myapp/public/javascripts/main.js
@@ -1,21 +1,26 @@
 
 var topnav = document.getElementsByClassName('topnav');
+var items = document.getElementsByClassName('items');
 function myFunction() {
     document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
 }
-// $(document).ready(function () {
-//     var scroll_start = 0;
-//     var startchange = $(".maintext");
-//     var offset = startchange.offset();
-//
-//     if (startchange.length) {
-//         $(document).scroll(function () {
-//             scroll_start = $(this).scrollTop();
-//             if (scroll_start > offset.top) {
-//                 $(topnav).css('background-color', 'rgba(0,0,0,.75)');
-//             } else {
-//                 $(topnav).css('background-color', 'transparent');
-//             }
-//         });
-//     }
-// });
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $(".maintext");
+    var offset = startchange.offset();
+
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $(topnav).css('background-color', 'rgba(355,355,355,.99)');
+                $(topnav).css('width', '100%');
+                $(items).css('float', 'right');
+
+            } else {
+                $(topnav).css('background-color', 'transparent');
+                // $(topnav).css('width', '100%');
+            }
+        });
+    }
+});
