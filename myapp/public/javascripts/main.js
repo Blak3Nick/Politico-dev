c0e43e1032fe09cc3eab91ed3d7994342bb48720
Blake Nicholson <blaken2021@gmail.com>
2016-08-10 17:28:10 -0400

    new wording

--- myapp/public/javascripts/main.js
+++ myapp/public/javascripts/main.js
@@ -3,19 +3,19 @@ var topnav = document.getElementsByClassName('topnav');
 function myFunction() {
     document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
 }
-$(document).ready(function () {
-    var scroll_start = 0;
-    var startchange = $(".maintext");
-    var offset = startchange.offset();
-
-    if (startchange.length) {
-        $(document).scroll(function () {
-            scroll_start = $(this).scrollTop();
-            if (scroll_start > offset.top) {
-                $(topnav).css('background-color', 'rgba(0,0,0,.75)');
-            } else {
-                $(topnav).css('background-color', 'transparent');
-            }
-        });
-    }
-});
+// $(document).ready(function () {
+//     var scroll_start = 0;
+//     var startchange = $(".maintext");
+//     var offset = startchange.offset();
+//
+//     if (startchange.length) {
+//         $(document).scroll(function () {
+//             scroll_start = $(this).scrollTop();
+//             if (scroll_start > offset.top) {
+//                 $(topnav).css('background-color', 'rgba(0,0,0,.75)');
+//             } else {
+//                 $(topnav).css('background-color', 'transparent');
+//             }
+//         });
+//     }
+// });
